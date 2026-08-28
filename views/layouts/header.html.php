32538aa09e5b5d78fffbe5bc8c997586597a1662
nainalaurent@gmail.com <Nainalaurent@gmail.com>
2026-08-28 09:05:02 +0100

    scss

--- views/layouts/header.html.php
+++ views/layouts/header.html.php
@@ -13,12 +13,9 @@
         <nav class="nav">
             <a href="/" class="nav-link">Acceuil</a>
             <a href="/?page=categories" class="nav-link">Categories</a>
-<<<<<<< HEAD
             <a href="/?page=produits" class="nav-link">Produits</a>
-=======
             <a href="/?page=about" class="nav-link">A propos</a>
             <a href="/?page=services" class="nav-link">Services</a>
->>>>>>> 239c5bceeec4fbefb2140056992ec41011906e5a
         </nav>
         
   
