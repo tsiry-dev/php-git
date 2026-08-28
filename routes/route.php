23b9b6ad7752139f5afb7987083b685ba5e1660b
nainalaurent@gmail.com <Nainalaurent@gmail.com>
2026-08-28 08:36:58 +0100

    scs

--- routes/route.php
+++ routes/route.php
@@ -6,13 +6,12 @@ if (isset($_GET['page'])) {
   $page = $_GET['page'];
 
   require_once PATH . '/routes/category.route.php';
-<<<<<<< HEAD
+
   require_once PATH . '/routes/produit.route.php';
-=======
+
   require_once PATH . '/routes/about.route.php';
   require_once PATH . '/routes/service.route.php';
 } else {
   homeController();
 }
->>>>>>> 239c5bceeec4fbefb2140056992ec41011906e5a
 
