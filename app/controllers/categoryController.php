7746b34bdc2708e86c5e2622457cf54464c9f468
tsiry <andriamiarijaonatsiri@gmail.com>
2026-08-28 08:22:00 +0300

    Add category list

--- app/controllers/categoryController.php
+++ app/controllers/categoryController.php
@@ -3,6 +3,22 @@
 
 function categoryController () {
 
+
+    $categories = [
+        [
+            "id" => "sdvhjbsd",
+            "title" => "PHP"
+        ],
+        [
+            "id" => "s5dv15ds1v",
+            "title" => "Javascript"
+        ],
+        [
+            "id" => "svcskdvsdv5116",
+            "title" => "Sass"
+        ],
+    ];
+
     require_once PATH . '/views/layouts/header.html.php';
     require_once PATH . '/views/category.html.php';
     require_once PATH . '/views/layouts/footer.html.php';
